6b9e9ea0c16aaf322b417ce7b2984c9ec556c387
Geoffrey Hayes <hayesgm@gmail.com>
2022-09-02 13:46:53 -0700

    Make sure Migrator is enabled

--- web/App.tsx
+++ web/App.tsx
@@ -3,6 +3,7 @@ import { SendRPC } from './lib/useRPC';
 import { read, write } from './lib/RPC';
 import { useEffect, useMemo, useState } from 'react';
 import ERC20 from '../abis/ERC20';
+import Comet from '../abis/Comet';
 
 import { JsonRpcProvider } from '@ethersproject/providers';
 
@@ -28,6 +29,7 @@ interface AppProps {
 
 interface AccountState {
   account?: string,
+  migratorEnabled: boolean,
   borrowBalanceV2?: BigInt
   cTokens: { [sym: string]: CTokenState }
 }
@@ -50,6 +52,7 @@ function showAmount(amount: BigInt, decimals: BigInt): string {
 export default ({sendRPC, web3}: AppProps) => {
   const [timer, setTimer] = useState(0);
   const initialAccountState = {
+    migratorEnabled: false,
     cTokens: Object.fromEntries(cTokenNames.map((cTokenName) => [cTokenName, { transfer: 0 }]))
   };
   const [accountState, setAccountState] = useState<AccountState>(initialAccountState);
@@ -63,7 +66,8 @@ export default ({sendRPC, web3}: AppProps) => {
       return [cTokenName, new Contract(mainnetV2Roots[cTokenName], mainnetV2Abi[cTokenName], signer)];
     }))}, [signer]);
 
-  const migrator = new Contract(cometV2MigratorAddress, cometV2MigratorAbi, web3);
+  const migrator = useMemo(() => new Contract(cometV2MigratorAddress, cometV2MigratorAbi, signer), [signer]);
+  const comet = useMemo(() => new Contract(mainnetV3Roots.comet, Comet, signer), [signer]);
 
   function setCTokenState(tokenSym: string, key: keyof CTokenState, value: CTokenState[key]) {
     console.log([tokenSym, key, value]);
@@ -86,6 +90,12 @@ export default ({sendRPC, web3}: AppProps) => {
     console.log("setting allowance");
     await cTokens[tokenSym].approve(migrator.address, MAX_UINT256);
     console.log("setting allowance");
+  }
+
+  async function enableMigrator() {
+    console.log("enabling migrator");
+    await comet.allow(migrator.address, true);
+    console.log("enabled migrator");
   }
 
   useEffect(() => {
@@ -112,6 +122,8 @@ export default ({sendRPC, web3}: AppProps) => {
           ...accountState,
           account
         });
+        let migratorEnabled = (await comet.allowance(account, migrator.address)).toBigInt() > 0n;
+        console.log({migratorEnabled});
         let tokenStates = Object.fromEntries(await Promise.all(Object.entries(accountState.cTokens).map<Promise<[string, CTokenState]>>(async ([sym, state]) => {
           return [`${sym}`, {
             ...state,
@@ -125,6 +137,7 @@ export default ({sendRPC, web3}: AppProps) => {
 
         setAccountState({
           account,
+          migratorEnabled,
           borrowBalanceV2: usdcBorrowsV2.toString(),
           cTokens: tokenStates
         });
@@ -136,11 +149,9 @@ export default ({sendRPC, web3}: AppProps) => {
     console.log("go", accountState);
   };
 
-  return (
-    <div className="container">
-      Compound II to Compound III Migrator<br/>
-      timer={ timer }<br/>
-      account={ accountState.account }<br/>
+  let el;
+  if (accountState.migratorEnabled) {
+    el = (<div>
       <div>
         { Object.entries(accountState.cTokens).map(([sym, state]) => {
           return <div key={`${sym}`}>
@@ -165,6 +176,19 @@ export default ({sendRPC, web3}: AppProps) => {
         })}
       </div>
       <button onClick={go}>Fire Trx</button>
+    </div>);
+  } else {
+    el = (<div>
+      <button onClick={enableMigrator}>Enable Migrator</button>
+    </div>);
+  }
+
+  return (
+    <div className="container">
+      Compound II to Compound III Migrator<br/>
+      timer={ timer }<br/>
+      account={ accountState.account }<br/>
+      { el }
     </div>
   );
 };
