e8d084d62700b115d9911916bacf5e4141552b6e
Geoffrey Hayes <hayesgm@gmail.com>
2022-09-12 18:31:31 -0700

    Start to add better, more thorough tests

--- test/Comet_V2_Migrator.t.sol
+++ test/Comet_V2_Migrator.t.sol
@@ -10,6 +10,12 @@ interface Comptroller {
 }
 
 contract ContractTest is Test {
+    struct Position {
+        CTokenLike collateral;
+        uint256 amount;
+    }
+    mapping (CTokenLike => address) holders;
+
     Comet public constant comet = Comet(0xc3d688B66703497DAA19211EEdff47f25384cdc3);
     IERC20 public constant usdc = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
     IERC20 public constant uni = IERC20(0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984);
@@ -21,64 +27,114 @@ contract ContractTest is Test {
     address public constant uniswapFactory = address(0x1F98431c8aD98523631AE4a59f267346ea31F984);
     IWETH9 public constant weth = IWETH9(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
     address public constant cHolderUni = address(0x39d8014b4F40d2CBC441137011d32023f4f1fd87);
-    address public constant caller = address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
+    address public constant cHolderEth = address(0xe84A061897afc2e7fF5FB7e3686717C528617487);
+    address public constant borrower = address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
     Comptroller public constant comptroller = Comptroller(0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B);
 
-    function setupMigratorBorrow() internal returns (Comet_V2_Migrator) {
+    function setUp() public {
+        holders[cUNI] = cHolderUni;
+        holders[cETH] = cHolderEth;
+    }
+
+    function setupMigratorBorrow(Position[] memory positions, uint256 borrowAmount) internal returns (Comet_V2_Migrator) {
         console.log("Deploying Comet v2 Migrator");
         Comet_V2_Migrator migrator = deployCometV2Migrator();
         console.log("Deployed Comet v2 Migrator", address(migrator));
 
-        setupV2Borrower(caller);
+        for (uint8 i = 0; i < positions.length; i++) {
+            setupV2Borrows(positions[i].collateral, positions[i].amount);
+        }
+
+        vm.prank(borrower);
+        require(cUSDC.borrow(borrowAmount) == 0, "failed to borrow"); // 100 USDC
+        require(usdc.balanceOf(borrower) == borrowAmount, "incorrect borrow");
+        require(cUSDC.borrowBalanceCurrent(borrower) >= borrowAmount, "incorrect borrow");
 
         return migrator;
     }
 
-    function testMigrateSimplePosition() public {
-        Comet_V2_Migrator migrator = setupMigratorBorrow();
+    function testMigrateSimpleUniPosition() public {
+        Position[] memory positions = new Position[](1);
+        positions[0] = Position({
+            collateral: cUNI,
+            amount: 200e18
+        });
+
+        Comet_V2_Migrator migrator = setupMigratorBorrow(positions, 700e6);
 
         Comet_V2_Migrator.Collateral[] memory collateral = new Comet_V2_Migrator.Collateral[](1);
         collateral[0] = Comet_V2_Migrator.Collateral({
             cToken: cUNI,
-            amount: 15000e8
+            amount: amountToTokens(199e18, cUNI)
         });
 
-        vm.prank(caller);
+        vm.prank(borrower);
         cUNI.approve(address(migrator), type(uint256).max);
 
-        vm.prank(caller);
+        vm.prank(borrower);
         comet.allow(address(migrator), true);
 
-        vm.prank(caller);
+        vm.prank(borrower);
         migrator.migrate(collateral, 600e6);
 
         // We should now have 15e18 less cUNI
-        require(cUNI.balanceOf(caller) == 5000e8, "Should have 5e18 cUNI after migration");
-        require(cUSDC.borrowBalanceCurrent(caller) < 101e6, "invalid borrows - should be less");
+        require(cUNI.balanceOf(borrower) == 5000e8, "Should have 5e18 cUNI after migration");
+        require(cUSDC.borrowBalanceCurrent(borrower) < 101e6, "invalid borrows - should be less");
 
         // Check v3 balances
-        console.log(comet.collateralBalanceOf(caller, address(uni)));
-        require(comet.collateralBalanceOf(caller, address(uni)) > 0, "check v3 collateral balance"); // this isn't right
-        require(comet.borrowBalanceOf(caller) >= 60e6, "check v3 borrow balance");// this isn't right
+        console.log(comet.collateralBalanceOf(borrower, address(uni)));
+        require(comet.collateralBalanceOf(borrower, address(uni)) > 0, "check v3 collateral balance"); // this isn't right
+        require(comet.borrowBalanceOf(borrower) >= 60e6, "check v3 borrow balance");// this isn't right
     }
 
-    function setupV2Borrower(address account) internal {
-        // Next, let's transfer in some cUNI to ourselves
-        vm.prank(cHolderUni);
-        cUNI.transfer(caller, 20000e8);
+    function testMigrateSimpleEthPosition() public {
+        Position[] memory positions = new Position[](1);
+        positions[0] = Position({
+            collateral: cETH,
+            amount: 1e18
+        });
 
-        require(cUNI.balanceOf(caller) == 20000e8, "invalid cUNI balance");
+        Comet_V2_Migrator migrator = setupMigratorBorrow(positions, 700e6);
 
-        // Next, we need to borrow cUSDC
-        vm.prank(account);
+        Comet_V2_Migrator.Collateral[] memory collateral = new Comet_V2_Migrator.Collateral[](1);
+        collateral[0] = Comet_V2_Migrator.Collateral({
+            cToken: cETH,
+            amount: 1e18
+        });
+
+        vm.prank(borrower);
+        cUNI.approve(address(migrator), type(uint256).max);
+
+        vm.prank(borrower);
+        comet.allow(address(migrator), true);
+
+        vm.prank(borrower);
+        migrator.migrate(collateral, 600e6);
+
+        // We should now have 15e18 less cUNI
+        require(cUNI.balanceOf(borrower) == 5000e8, "Should have 5e18 cUNI after migration");
+        require(cUSDC.borrowBalanceCurrent(borrower) < 101e6, "invalid borrows - should be less");
+
+        // Check v3 balances
+        console.log(comet.collateralBalanceOf(borrower, address(uni)));
+        require(comet.collateralBalanceOf(borrower, address(uni)) > 0, "check v3 collateral balance"); // this isn't right
+        require(comet.borrowBalanceOf(borrower) >= 60e6, "check v3 borrow balance");// this isn't right
+    }
+
+    function setupV2Borrows(CTokenLike cToken, uint256 amount) internal {
+        // Next, let's transfer in some of the cToken to ourselves
+        uint256 tokens = amountToTokens(amount, cToken);
+        console.log(address(cToken), tokens);
+        vm.prank(holders[cToken]);
+        cToken.transfer(borrower, tokens);
+
+        require(cToken.balanceOf(borrower) == tokens, "invalid cToken balance");
+
+        // Next, we need to enter this market
+        vm.prank(borrower);
         address[] memory markets = new address[](1);
-        markets[0] = address(cUNI);
+        markets[0] = address(cToken);
         comptroller.enterMarkets(markets);
-
-        vm.prank(account);
-        require(cUSDC.borrow(700e6) == 0, "failed to borrow"); // 100 USDC
-        require(usdc.balanceOf(account) == 700e6, "incorrect borrow");
-        require(cUSDC.borrowBalanceCurrent(account) >= 700e6, "incorrect borrow");
     }
 
     function deployCometV2Migrator() internal returns (Comet_V2_Migrator) {
@@ -90,5 +146,9 @@ contract ContractTest is Test {
             pool_DAI_USDC,
             sweepee
         );
+    }
+
+    function amountToTokens(uint256 amount, CTokenLike cToken) internal returns (uint256) {
+        return ( 1e18 * amount ) / cToken.exchangeRateCurrent();
     }
 }
