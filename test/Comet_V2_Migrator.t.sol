e5ff51f43205e9e3d72dc8ce6fb419b5c81134f7
Geoffrey Hayes <hayesgm@gmail.com>
2022-09-12 17:41:57 -0700

    Support cETH
    This patch starts to add support for cETH to both the spec and the code. This is necessary since cETH will be a common collateral token.
    
    We also start to build the sweep function.
    
    We also start to implement `inMigration`.
    
    We also slim out Uniswap libraries.
    
    We also start to fix token approvals and remove the "enumerated collateral tokens" concept.

--- test/Comet_V2_Migrator.t.sol
+++ test/Comet_V2_Migrator.t.sol
@@ -15,25 +15,28 @@ contract ContractTest is Test {
     IERC20 public constant uni = IERC20(0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984);
     CErc20 public constant cUSDC = CErc20(0x39AA39c021dfbaE8faC545936693aC917d5E7563);
     CErc20 public constant cUNI = CErc20(0x35A18000230DA775CAc24873d00Ff85BccdeD550);
+    CTokenLike public constant cETH = CTokenLike(0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5);
     IUniswapV3Pool public constant pool_DAI_USDC = IUniswapV3Pool(0x5777d92f208679DB4b9778590Fa3CAB3aC9e2168);
     address payable public constant sweepee = payable(0x6d903f6003cca6255D85CcA4D3B5E5146dC33925);
     address public constant uniswapFactory = address(0x1F98431c8aD98523631AE4a59f267346ea31F984);
-    address public constant WETH9 = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    IWETH9 public constant weth = IWETH9(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
     address public constant cHolderUni = address(0x39d8014b4F40d2CBC441137011d32023f4f1fd87);
     address public constant caller = address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
     Comptroller public constant comptroller = Comptroller(0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B);
 
-    Comet_V2_Migrator public migrator;
-
-    function setUp() public {
+    function setupMigratorBorrow() internal returns (Comet_V2_Migrator) {
         console.log("Deploying Comet v2 Migrator");
-        migrator = deployCometV2Migrator();
+        Comet_V2_Migrator migrator = deployCometV2Migrator();
         console.log("Deployed Comet v2 Migrator", address(migrator));
 
-        setupV2Borrower(migrator, caller);
+        setupV2Borrower(caller);
+
+        return migrator;
     }
 
     function testMigrateSimplePosition() public {
+        Comet_V2_Migrator migrator = setupMigratorBorrow();
+
         Comet_V2_Migrator.Collateral[] memory collateral = new Comet_V2_Migrator.Collateral[](1);
         collateral[0] = Comet_V2_Migrator.Collateral({
             cToken: cUNI,
@@ -59,9 +62,8 @@ contract ContractTest is Test {
         require(comet.borrowBalanceOf(caller) >= 60e6, "check v3 borrow balance");// this isn't right
     }
 
-    function setupV2Borrower(Comet_V2_Migrator migrator, address account) internal {
+    function setupV2Borrower(address account) internal {
         // Next, let's transfer in some cUNI to ourselves
-        uint256 cUNIBalance = cUNI.balanceOf(cHolderUni);
         vm.prank(cHolderUni);
         cUNI.transfer(caller, 20000e8);
 
@@ -80,16 +82,13 @@ contract ContractTest is Test {
     }
 
     function deployCometV2Migrator() internal returns (Comet_V2_Migrator) {
-        IERC20[] memory tokens = new IERC20[](0);
-
         return new Comet_V2_Migrator(
             comet,
             cUSDC,
+            cETH,
+            weth,
             pool_DAI_USDC,
-            tokens,
-            sweepee,
-            uniswapFactory,
-            WETH9
+            sweepee
         );
     }
 }
