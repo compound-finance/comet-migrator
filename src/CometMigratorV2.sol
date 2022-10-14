91b3097634d3ea129be51ad5c4cae6ea34d829b2
kevincheng96 <kevincheng96@hotmail.com>
2022-10-14 11:49:42 -0700

    Implement Migrator V2 from Compound V2 and split out v2 into its own contract/tests

--- src/CometMigratorV2.sol
+++ src/CometMigratorV2.sol
@@ -6,13 +6,12 @@ import "./vendor/@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
 import "./vendor/@uniswap/v3-periphery/contracts/interfaces/external/IWETH9.sol";
 import "./vendor/@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 import "./vendor/@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "./interfaces/AaveInterface.sol";
 import "./interfaces/CTokenInterface.sol";
 import "./interfaces/CometInterface.sol";
 
 /**
- * @title Compound III Migrator v2
- * @notice A contract to help migrate a Compound II or Aave v2 position into a similar Compound III position.
+ * @title Compound V3 Migrator v2
+ * @notice A contract to help migrate a Compound v2 position or other DeFi position into a similar Compound v3 position.
  * @author Compound
  */
 contract CometMigratorV2 is IUniswapV3FlashCallback {
@@ -28,45 +27,25 @@ contract CometMigratorV2 is IUniswapV3FlashCallback {
   event Migrated(
     address indexed user,
     CompoundV2Position compoundV2Position,
-    AaveV2Position aaveV2Position,
     uint256 flashAmount,
     uint256 flashAmountWithFee);
 
-  /// @notice Represents an entire Compound II position (collateral + borrows) to migrate.
+  /// @notice Represents an entire Compound V2 position (collateral + borrows) to migrate.
   struct CompoundV2Position {
     CompoundV2Collateral[] collateral;
     CompoundV2Borrow[] borrows;
     bytes[] paths; // empty path if no swap is required (e.g. repaying USDC borrow)
   }
 
-  /// @notice Represents a given amount of Compound II collateral to migrate.
+  /// @notice Represents a given amount of Compound V2 collateral to migrate.
   struct CompoundV2Collateral {
     CTokenLike cToken;
-    uint256 amount; // Note: This is the amount of the cToken
-  }
-
-  /// @notice Represents a given amount of Compound II borrow to migrate.
-  struct CompoundV2Borrow {
-    CErc20 cToken;
-    uint256 amount; // Note: This is the amount of the underlying, not the cToken
-  }
-
-  /// @notice Represents an entire Aave v2 position (collateral + borrows) to migrate.
-  struct AaveV2Position {
-    AaveV2Collateral[] collateral;
-    AaveV2Borrow[] borrows;
-    bytes[] paths; // empty path if no swap is required (e.g. repaying USDC borrow)
-  }
-
-  /// @notice Represents a given amount of Aave v2 collateral to migrate.
-  struct AaveV2Collateral {
-    ATokenLike aToken;
     uint256 amount;
   }
 
-  /// @notice Represents a given amount of Aave v2 borrow to migrate.
-  struct AaveV2Borrow {
-    ADebtTokenLike aDebtToken; // Note: Aave has two separate debt tokens per asset: stable and variable rate
+  /// @notice Represents a given amount of Compound V2 borrow to migrate.
+  struct CompoundV2Borrow {
+    CErc20 cToken;
     uint256 amount;
   }
 
@@ -75,7 +54,6 @@ contract CometMigratorV2 is IUniswapV3FlashCallback {
     address user;
     uint256 flashAmount;
     CompoundV2Position compoundV2Position;
-    AaveV2Position aaveV2Position;
   }
 
   /// @notice The Comet Ethereum mainnet USDC contract
@@ -99,9 +77,6 @@ contract CometMigratorV2 is IUniswapV3FlashCallback {
   /// @notice The address of the `weth` token.
   IWETH9 public immutable weth;
 
-  /// @notice The address of the Aave v2 LendingPool contract. This is the contract that all `withdraw` and `repay` transactions go through.
-  ILendingPool public immutable aaveV2LendingPool;
-
   /// @notice Address to send swept tokens to, if for any reason they remain locked in this contract.
   address payable public immutable sweepee;
 
@@ -109,12 +84,11 @@ contract CometMigratorV2 is IUniswapV3FlashCallback {
   uint256 public inMigration;
 
   /**
-   * @notice Construct a new CometMigratorV2
+   * @notice Construct a new Compound_Migrate_V2_USDC_to_V3_USDC
    * @param comet_ The Comet Ethereum mainnet USDC contract.
    * @param baseToken_ The base token of the Compound III market (e.g. `USDC`).
    * @param cETH_ The address of the `cETH` token.
    * @param weth_ The address of the `WETH9` token.
-   * @param aaveV2LendingPool_ The address of the Aave v2 LendingPool contract. This is the contract that all `withdraw` and `repay` transactions go through.
    * @param uniswapLiquidityPool_ The Uniswap pool used by this contract to source liquidity (i.e. flash loans).
    * @param swapRouter_ The Uniswap router for facilitating token swaps.
    * @param sweepee_ Sweep excess tokens to this address.
@@ -124,7 +98,6 @@ contract CometMigratorV2 is IUniswapV3FlashCallback {
     IERC20 baseToken_,
     CTokenLike cETH_,
     IWETH9 weth_,
-    ILendingPool aaveV2LendingPool_,
     IUniswapV3Pool uniswapLiquidityPool_,
     ISwapRouter swapRouter_,
     address payable sweepee_
@@ -140,9 +113,6 @@ contract CometMigratorV2 is IUniswapV3FlashCallback {
 
     // **WRITE IMMUTABLE** `weth = weth_`
     weth = weth_;
-
-    // **WRITE IMMUTABLE** `aaveV2LendingPool = aaveV2LendingPool_`
-    aaveV2LendingPool = aaveV2LendingPool_;
 
     // **WRITE IMMUTABLE** `uniswapLiquidityPool = uniswapLiquidityPool_`
     uniswapLiquidityPool = uniswapLiquidityPool_;
@@ -167,15 +137,14 @@ contract CometMigratorV2 is IUniswapV3FlashCallback {
 
   /**
    * @notice This is the core function of this contract, migrating a position from Compound II to Compound III. We use a flash loan from Uniswap to provide liquidity to move the position.
-   * @param compoundV2Position Structure containing the user’s Compound II collateral and borrow positions to migrate to Compound III. See notes below.
-   * @param aaveV2Position Structure containing the user’s Compound II collateral and borrow positions to migrate to Compound III. See notes below.
+   * @param compoundV2Position Structure containing the user’s Compound V2 collateral and borrow positions to migrate to Compound III. See notes below.
    * @param flashAmount Amount of base asset to borrow from the Uniswap flash loan to facilitate the migration. See notes below.
    * @dev **N.B.** Collateral requirements may be different in Compound II and Compound III. This may lead to a migration failing or being less collateralized after the migration. There are fees associated with the flash loan, which may affect position or cause migration to fail.
    * @dev Note: each `collateral` market must be supported in Compound III.
    * @dev Note: `collateral` amounts of 0 are strictly ignored. Collateral amounts of max uint256 are set to the user's current balance.
    * @dev Note: `flashAmount` is provided by the user as a hint to the Migrator to know the maximum expected cost (in terms of the base asset) of the migration. If `flashAmount` is less than the total amount needed to migrate the user’s positions, the transaction will revert.
    **/
-  function migrate(CompoundV2Position calldata compoundV2Position, AaveV2Position calldata aaveV2Position, uint256 flashAmount) external {
+  function migrate(CompoundV2Position calldata compoundV2Position, uint256 flashAmount) external {
     // **REQUIRE** `inMigration == 0`
     if (inMigration != 0) {
       revert Reentrancy(0);
@@ -192,17 +161,11 @@ contract CometMigratorV2 is IUniswapV3FlashCallback {
       revert InvalidInputs(0);
     }
 
-    // **REQUIRE** `aaveV2Position.borrows.length == aaveV2Position.paths.length`
-    if (aaveV2Position.borrows.length != aaveV2Position.paths.length) {
-      revert InvalidInputs(1);
-    }
-
-    // **BIND** `data = abi.encode(MigrationCallbackData{user, flashAmount, compoundV2Position, aaveV2Position, makerPositions})`
+    // **BIND** `data = abi.encode(MigrationCallbackData{user, flashAmount, compoundV2Position, avveV2Position, makerPositions})`
     bytes memory data = abi.encode(MigrationCallbackData({
       user: user,
       flashAmount: flashAmount,
-      compoundV2Position: compoundV2Position,
-      aaveV2Position: aaveV2Position
+      compoundV2Position: compoundV2Position
     }));
 
     // **CALL** `uniswapLiquidityPool.flash(address(this), isUniswapLiquidityPoolToken0 ? flashAmount : 0, isUniswapLiquidityPoolToken0 ? 0 : flashAmount, data)`
@@ -216,7 +179,7 @@ contract CometMigratorV2 is IUniswapV3FlashCallback {
    * @notice This function handles a callback from the Uniswap Liquidity Pool after it has sent this contract the requested tokens. We are responsible for repaying those tokens, with a fee, before we return from this function call.
    * @param fee0 The fee for borrowing token0 from pool.
    * @param fee1 The fee for borrowing token1 from pool.
-   * @param data The data encoded above, which is the ABI-encoding of `MigrationCallbackData`.
+   * @param data The data encoded above, which is the ABI-encoding of XXX.
    **/
   function uniswapV3FlashCallback(uint256 fee0, uint256 fee1, bytes calldata data) external {
     // **REQUIRE** `inMigration == 1`
@@ -229,7 +192,7 @@ contract CometMigratorV2 is IUniswapV3FlashCallback {
       revert InvalidCallback(0);
     }
 
-    // **BIND** `MigrationCallbackData{user, flashAmount, compoundV2Position, aaveV2Position, cdpPositions} = abi.decode(data, (MigrationCallbackData))`
+    // **BIND** `MigrationCallbackData{user, flashAmount, compoundV2Position, avveV2Position, cdpPositions} = abi.decode(data, (MigrationCallbackData))`
     MigrationCallbackData memory migrationData = abi.decode(data, (MigrationCallbackData));
 
     // **BIND** `flashAmountWithFee = flashAmount + isUniswapLiquidityPoolToken0 ? fee0 : fee1`
@@ -238,9 +201,6 @@ contract CometMigratorV2 is IUniswapV3FlashCallback {
     // **EXEC** `migrateCompoundV2Position(user, compoundV2Position)`
     migrateCompoundV2Position(migrationData.user, migrationData.compoundV2Position);
 
-    // **EXEC** `migrateAaveV2Position(user, aaveV2Position)`
-    migrateAaveV2Position(migrationData.user, migrationData.aaveV2Position);
-
     // **CALL** `comet.withdrawFrom(user, address(this), baseToken, flashAmountWithFee - baseToken.balanceOf(address(this)))`
     comet.withdrawFrom(migrationData.user, address(this), address(baseToken), flashAmountWithFee - baseToken.balanceOf(address(this)));
 
@@ -248,13 +208,13 @@ contract CometMigratorV2 is IUniswapV3FlashCallback {
     baseToken.transfer(address(uniswapLiquidityPool), flashAmountWithFee);
 
     // **EMIT** `Migrated(user, compoundV2Position, aaveV2Position, cdpPositions, flashAmount, flashAmountWithFee)`
-    emit Migrated(migrationData.user, migrationData.compoundV2Position, migrationData.aaveV2Position, migrationData.flashAmount, flashAmountWithFee);
+    emit Migrated(migrationData.user, migrationData.compoundV2Position, migrationData.flashAmount, flashAmountWithFee);
   }
 
   /**
-   * @notice This internal helper function repays the user’s borrow positions on Compound II (executing swaps first if necessary) before migrating their collateral over to Compound III.
+   * @notice This internal helper function repays the user’s borrow positions on Compound V2 (executing swaps first if necessary) before migrating their collateral over to Compound III.
    * @param user Alias for the `msg.sender` of the original `migrate` call.
-   * @param position Structure containing the user’s Compound II collateral and borrow positions to migrate to Compound III.
+   * @param position Structure containing the user’s Compound V2 collateral and borrow positions to migrate to Compound III.
    **/
   function migrateCompoundV2Position(address user, CompoundV2Position memory position) internal {
     // **FOREACH** `(cToken, borrowAmount): CompoundV2Borrow, path: bytes` in `position`:
@@ -282,6 +242,7 @@ contract CometMigratorV2 is IUniswapV3FlashCallback {
               deadline: block.timestamp
           })
         );
+        // XXX keep a running counter of how much borrowed asset is left? (subtract `amountIn` from `flashAmount`)
       }
 
       // **CALL** `cToken.underlying().approve(address(cToken), repayAmount)`
@@ -296,9 +257,8 @@ contract CometMigratorV2 is IUniswapV3FlashCallback {
 
     // **FOREACH** `(cToken, amount): CompoundV2Collateral` in `position.collateral`:
     for (uint i = 0; i < position.collateral.length; i++) {
-      CompoundV2Collateral memory collateral = position.collateral[i];
-
       // **CALL** `cToken.transferFrom(user, address(this), amount == type(uint256).max ? cToken.balanceOf(user) : amount)`
+      CompoundV2Collateral memory collateral = position.collateral[i];
       bool transferSuccess = collateral.cToken.transferFrom(
         user,
         address(this),
@@ -336,80 +296,6 @@ contract CometMigratorV2 is IUniswapV3FlashCallback {
         user,
         address(underlying),
         underlying.balanceOf(address(this))
-      );
-    }
-  }
-
-  /**
-   * @notice This internal helper function repays the user’s borrow positions on Aave v2 (executing swaps first if necessary) before migrating their collateral over to Compound III.
-   * @param user Alias for the `msg.sender` of the original `migrate` call.
-   * @param position Structure containing the user’s Aave v2 collateral and borrow positions to migrate to Compound III.
-   **/
-  function migrateAaveV2Position(address user, AaveV2Position memory position) internal {
-    // **FOREACH** `(aDebtToken, borrowAmount): AaveV2Borrow, path: bytes` in `position`:
-    for (uint i = 0; i < position.borrows.length; i++) {
-      AaveV2Borrow memory borrow = position.borrows[i];
-      uint256 repayAmount;
-      //  **WHEN** `borrowAmount == type(uint256).max)`:
-      if (borrow.amount == type(uint256).max) {
-        // **BIND READ** `repayAmount = aDebtToken.balanceOf(user)`
-        repayAmount = borrow.aDebtToken.balanceOf(user);
-      } else {
-        //  **BIND** `repayAmount = borrowAmount`
-        repayAmount = borrow.amount;
-      }
-      // **WHEN** `path.length > 0`:
-      if (position.paths[i].length > 0) {
-        //  **CALL** `ISwapRouter.exactOutput(ExactOutputParams({path: path, recipient: address(this), amountOut: repayAmount, amountInMaximum: type(uint256).max})`
-        uint256 amountIn = swapRouter.exactOutput(
-          ISwapRouter.ExactOutputParams({
-              path: position.paths[i],
-              recipient: address(this),
-              amountOut: repayAmount,
-              amountInMaximum: type(uint256).max,
-              deadline: block.timestamp
-          })
-        );
-      }
-
-      // **BIND READ** `underlyingDebt = aDebtToken.UNDERLYING_ASSET_ADDRESS()`
-      IERC20 underlyingDebt = IERC20(borrow.aDebtToken.UNDERLYING_ASSET_ADDRESS());
-
-      // **BIND READ** `rateMode = aDebtToken.DEBT_TOKEN_REVISION()`
-      uint256 rateMode = borrow.aDebtToken.DEBT_TOKEN_REVISION();
-
-      // **CALL** `underlyingDebt.approve(address(aaveV2LendingPool), repayAmount)`
-      underlyingDebt.approve(address(aaveV2LendingPool), repayAmount);
-
-      // **CALL** `aaveV2LendingPool.repay(underlyingDebt, repayAmount, rateMode, user)`
-      aaveV2LendingPool.repay(address(underlyingDebt), repayAmount, rateMode, user);
-    }
-
-    // **FOREACH** `(aToken, amount): AaveV2Collateral` in `position.collateral`:
-    for (uint i = 0; i < position.collateral.length; i++) {
-      AaveV2Collateral memory collateral = position.collateral[i];
-
-      // **CALL** `aToken.transferFrom(user, address(this), amount == type(uint256).max ? aToken.balanceOf(user) : amount)`
-      collateral.aToken.transferFrom(
-        user,
-        address(this),
-        collateral.amount == type(uint256).max ? collateral.aToken.balanceOf(user) : collateral.amount
-      );
-
-      // **BIND READ** `underlyingCollateral = aToken.UNDERLYING_ASSET_ADDRESS()`
-      IERC20 underlyingCollateral = IERC20(collateral.aToken.UNDERLYING_ASSET_ADDRESS());
-
-      // **CALL** `aaveV2LendingPool.withdraw(underlyingCollateral, aToken.balanceOf(address(this)), address(this))`
-      aaveV2LendingPool.withdraw(address(underlyingCollateral), collateral.aToken.balanceOf(address(this)), address(this));
-
-      // **CALL** `underlyingCollateral.approve(address(comet), type(uint256).max)`
-      underlyingCollateral.approve(address(comet), type(uint256).max);
-
-      // **CALL** `comet.supplyTo(user, underlyingCollateral, underlyingCollateral.balanceOf(address(this)))`
-      comet.supplyTo(
-        user,
-        address(underlyingCollateral),
-        underlyingCollateral.balanceOf(address(this))
       );
     }
   }
--- test/PositorV2.t.sol
+++ test/PositorV2.t.sol
@@ -6,29 +6,19 @@ import "../src/CometMigratorV2.sol";
 import "forge-std/Test.sol";
 import "./MainnetConstants.t.sol";
 
-contract Positor is Test, MainnetConstants {
-    // XXX change name to `PositCompoundV2`?
+contract PositorV2 is Test, MainnetConstants {
     struct Posit {
         address borrower;
         CometMigratorV2.CompoundV2Collateral[] collateral;
         CometMigratorV2.CompoundV2Borrow[] borrows;
     }
 
-    struct PositAaveV2 {
-        address borrower;
-        CometMigratorV2.AaveV2Collateral[] collateral;
-        CometMigratorV2.AaveV2Borrow[] borrows;
-    }
-
-    mapping (CTokenLike => address) cTokenHolders;
-    mapping (ATokenLike => address) aTokenHolders;
+    mapping (CTokenLike => address) holders;
     CometMigratorV2 public immutable migrator;
 
     constructor() {
-        cTokenHolders[cUNI] = cHolderUni;
-        cTokenHolders[cETH] = cHolderEth;
-        aTokenHolders[aUNI] = aHolderUni;
-        aTokenHolders[aWETH] = aHolderWeth;
+        holders[cUNI] = cHolderUni;
+        holders[cETH] = cHolderEth;
 
         console.log("Deploying Comet Migrator");
         migrator = deployCometMigrator();
@@ -36,37 +26,32 @@ contract Positor is Test, MainnetConstants {
     }
 
     function posit(Posit memory posit_) public {
-        setupCompoundV2MigratorBorrow(posit_.borrower, posit_.collateral, posit_.borrows);
+        setupMigratorBorrow(posit_.borrower, posit_.collateral, posit_.borrows);
     }
 
-    function positAaveV2(PositAaveV2 memory posit_) public {
-        setupAaveV2MigratorBorrow(posit_.borrower, posit_.collateral, posit_.borrows);
-    }
-
-    function setupCompoundV2MigratorBorrow(address borrower, CometMigratorV2.CompoundV2Collateral[] memory collateral, CometMigratorV2.CompoundV2Borrow[] memory borrows) internal returns (CometMigratorV2) {
+    function setupMigratorBorrow(address borrower, CometMigratorV2.CompoundV2Collateral[] memory collateral, CometMigratorV2.CompoundV2Borrow[] memory borrows) internal returns (CometMigratorV2) {
         for (uint8 i = 0; i < collateral.length; i++) {
-            setupCompoundV2Collateral(borrower, collateral[i].cToken, collateral[i].amount);
+            setupV2Borrows(borrower, collateral[i].cToken, collateral[i].amount);
         }
 
         for (uint8 i = 0; i < borrows.length; i++) {
             CErc20 cToken = borrows[i].cToken;
             IERC20 underlying = cToken.underlying(); // XXX doesn't work for cETH
-            uint256 preUnderlyingAmount = underlying.balanceOf(borrower);
             uint256 borrowAmount = borrows[i].amount;
             vm.prank(borrower);
             require(cToken.borrow(borrowAmount) == 0, "failed to borrow"); // 100 USDC
-            require(underlying.balanceOf(borrower) - preUnderlyingAmount == borrowAmount, "incorrect borrow");
+            require(underlying.balanceOf(borrower) == borrowAmount, "incorrect borrow");
             require(cToken.borrowBalanceCurrent(borrower) >= borrowAmount, "incorrect borrow");
         }
 
         return migrator;
     }
 
-    function setupCompoundV2Collateral(address borrower, CTokenLike cToken, uint256 amount) internal {
+    function setupV2Borrows(address borrower, CTokenLike cToken, uint256 amount) internal {
         // Next, let's transfer in some of the cToken to ourselves
         uint256 tokens = amountToTokens(amount, cToken);
         console.log(address(cToken), tokens);
-        vm.prank(cTokenHolders[cToken]);
+        vm.prank(holders[cToken]);
         cToken.transfer(borrower, tokens);
 
         require(cToken.balanceOf(borrower) == tokens, "invalid cToken balance");
@@ -78,44 +63,12 @@ contract Positor is Test, MainnetConstants {
         comptroller.enterMarkets(markets);
     }
 
-    function setupAaveV2MigratorBorrow(address borrower, CometMigratorV2.AaveV2Collateral[] memory collateral, CometMigratorV2.AaveV2Borrow[] memory borrows) internal returns (CometMigratorV2) {
-        for (uint8 i = 0; i < collateral.length; i++) {
-            setupAaveV2Collateral(borrower, collateral[i].aToken, collateral[i].amount);
-        }
-
-        for (uint8 i = 0; i < borrows.length; i++) {
-            ADebtTokenLike aDebtToken = borrows[i].aDebtToken;
-            IERC20 underlying = IERC20(aDebtToken.UNDERLYING_ASSET_ADDRESS());
-            uint256 preUnderlyingAmount = underlying.balanceOf(borrower);
-            uint256 borrowAmount = borrows[i].amount;
-            vm.prank(borrower);
-            aDebtToken.approveDelegation(address(this), type(uint256).max);
-            vm.prank(borrower); // XXX prank not using the correct msg.sender for borrow, so we approveDelegation above first
-            aaveV2LendingPool.borrow(address(underlying), borrowAmount, aDebtToken.DEBT_TOKEN_REVISION(), 0, borrower);
-            underlying.transfer(borrower, borrowAmount);
-            require(underlying.balanceOf(borrower) - preUnderlyingAmount == borrowAmount, "incorrect borrow");
-            require(aDebtToken.balanceOf(borrower) >= borrowAmount, "incorrect borrow");
-        }
-
-        return migrator;
-    }
-
-    function setupAaveV2Collateral(address borrower, ATokenLike aToken, uint256 amount) internal {
-        // Next, let's transfer in some of the aToken to ourselves
-        console.log(address(aToken), amount);
-        vm.prank(aTokenHolders[aToken]);
-        aToken.transfer(borrower, amount);
-
-        require(aToken.balanceOf(borrower) == amount, "invalid aToken balance");
-    }
-
     function deployCometMigrator() internal returns (CometMigratorV2) {
         return new CometMigratorV2(
             comet,
             usdc,
             cETH,
             weth,
-            aaveV2LendingPool,
             pool_DAI_USDC,
             swapRouter,
             sweepee
