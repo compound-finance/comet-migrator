248888cfcaceb84e83bec789f8ccffbec13eb652
Geoffrey Hayes <hayesgm@gmail.com>
2022-10-21 00:09:15 -0700

    Disable buttons while we have pending transactions

--- web/lib/useTransactionTracker.ts
+++ web/lib/useTransactionTracker.ts
@@ -2,15 +2,19 @@ import type { TransactionReceipt, TransactionResponse } from '@ethersproject/pro
 import { useMemo, useState, useEffect } from 'react';
 import { JsonRpcProvider } from '@ethersproject/providers';
 
-export type Status = Map<string, null | TransactionReceipt>;
+export type Tracker = Map<string, null | TransactionReceipt>;
+
+export function hasPendingTransaction(tracker: Tracker): boolean {
+  return [...tracker.entries()].some(([hash, status]) => status === null);
+}
 
 export function useTransactionTracker(web3: JsonRpcProvider) {
-  let [tracker, setStatus] = useState<Status>(new Map());
+  let [tracker, setTracker] = useState<Tracker>(new Map());
 
   function setTxStatus(txHash: string, txStatus: null | TransactionReceipt) {
     let nextTracker = new Map(tracker);
     nextTracker.set(txHash, txStatus);
-    setStatus(nextTracker);
+    setTracker(nextTracker);
   }
 
   function trackTransaction(responsePromise: Promise<TransactionResponse>): Promise<TransactionResponse> {
