50a2758e3ba2c584bde2f4b93edc80b84d989335
kevincheng96 <kevincheng96@hotmail.com>
2022-11-30 14:38:30 -0800

    Add CometMigratorV2 deploy script and update Goerli comet address

--- script/mainnet/deploy_migrator_v2.sh
+++ script/mainnet/deploy_migrator_v2.sh
@@ -22,32 +22,38 @@ fi
 
 # Constructor Variables
 #
-# CometMigrator::constructor(
+# CometMigratorV2::constructor(
 #   comet_ :: The Comet Ethereum mainnet USDC contract.
-#   borrowCToken_ :: The Compound II market for the borrowed token (e.g. `cUSDC`).
+#   baseToken_ :: The base token of the Compound III market (e.g. `USDC`).
 #   cETH_ :: The address of the `cETH` token.
 #   weth_ :: The address of the `WETH9` token.
+#   aaveV2LendingPool_ :: The address of the Aave v2 LendingPool contract. This is the contract that all `withdraw` and `repay` transactions go through.
 #   uniswapLiquidityPool_ :: The Uniswap pool used by this contract to source liquidity (i.e. flash loans).
+#   swapRouter_ :: The Uniswap router for facilitating token swaps.
 #   sweepee_ :: Sweep excess tokens to this address.
 # )
 #
-comet="0x3EE77595A8459e93C2888b13aDB354017B198188"
-borrowCToken="0x73506770799Eb04befb5AaE4734e58C2C624F493"
-cETH="0x64078a6189Bf45f80091c6Ff2fCEe1B15Ac8dbde"
-weth="0x42a71137C09AE83D8d05974960fd607d40033499"
-uniswapLiquidityPool="0x9288451589Bfd7d6bEAd87d9D6689FB8Dec7ddAa"
-sweepee="0xbBFE34E868343E6F4f5E8B5308de980d7bd88c46"
+comet="0xc3d688B66703497DAA19211EEdff47f25384cdc3"
+baseToken="0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+cETH="0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5"
+weth="0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+aaveV2LendingPool="0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9"
+uniswapLiquidityPool="0x5777d92f208679DB4b9778590Fa3CAB3aC9e2168"
+swapRouter="0xE592427A0AEce92De3Edee1F18E0157C05861564"
+sweepee="0x6d903f6003cca6255D85CcA4D3B5E5146dC33925"
 
 forge create \
   $rpc_args \
   $etherscan_args \
   $wallet_args \
   $@ \
-  src/CometMigrator.sol:CometMigrator \
+  src/CometMigratorV2.sol:CometMigratorV2 \
   --constructor-args \
     "$comet" \
-    "$borrowCToken" \
+    "$baseToken" \
     "$cETH" \
     "$weth" \
+    "$aaveV2LendingPool" \
     "$uniswapLiquidityPool" \
+    "$swapRouter" \
     "$sweepee"
