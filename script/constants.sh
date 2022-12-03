419436ca21841a5fb6e76d15e0e1c26bbeadff00
Mykel Pereira <me@mykelpereira.com>
2022-12-02 22:04:15 -0600

    Move back fork_block to fix coverage

--- script/constants.sh
+++ script/constants.sh
@@ -5,7 +5,7 @@ function set_constants() {
   export ETH_PRIVATE_KEY="0x10f211fde09d6878860bafdbc9ddb2fbd676d489c07eb2c89618dc588ec6bf68"
   export fork_url="${ETHEREUM_REMOTE_NODE_MAINNET:-https://mainnet-eth.compound.finance/}"
   export mnemonic="panel capable wet impulse ozone asset forget stamp stand long nose talk"
-  export fork_block="16099990"
+  export fork_block="15542274"
   export v1_mainnet_deploy_block="15749484"
 
   [ -f ".env" ] && source .env
