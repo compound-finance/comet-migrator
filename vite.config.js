f12cf8c21d9cf763ed8917c5d3ae46a769270692
Mykel Pereira <me@mykelpereira.com>
2022-12-06 22:02:22 -0800

    commonjs rollup

--- vite.config.js
+++ vite.config.js
@@ -2,57 +2,58 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
-import nodePolyfills from "rollup-plugin-polyfill-node";
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   // Node.js global to browser globalThis
   define: {
-    global: "globalThis",
+    global: 'globalThis'
   },
   server: {
     port: 5183,
-    strictPort: true,
+    strictPort: true
   },
   build: {
     target: ['es2020'],
     rollupOptions: {
       input: {
         index: resolve(__dirname, 'index.html'),
-        embedded: resolve(__dirname, 'embedded.html'),
+        embedded: resolve(__dirname, 'embedded.html')
       },
-      plugins: [nodePolyfills(), nodeResolve()],
-    },
+      plugins: [commonjs(), nodePolyfills(), nodeResolve()]
+    }
   },
   base: '',
   publicDir: 'web_public',
   experimental: {
     renderBuiltUrl: (filename, { hostType }) => {
       return { relative: true };
-    },
+    }
   },
   resolve: {
     alias: {
       events: 'rollup-plugin-node-polyfills/polyfills/events',
-      '/fonts': resolve(__dirname, 'node_modules/compound-styles/public/fonts'),
-    },
+      '/fonts': resolve(__dirname, 'node_modules/compound-styles/public/fonts')
+    }
   },
   optimizeDeps: {
     esbuildOptions: {
       // Node.js global to browser globalThis
       define: {
-        global: 'globalThis',
+        global: 'globalThis'
       },
       // Enable esbuild polyfill plugins
       plugins: [
         NodeGlobalsPolyfillPlugin({
-          buffer: true,
+          buffer: true
         }),
         NodeModulesPolyfillPlugin()
-      ],
-    },
-  },
+      ]
+    }
+  }
 });
